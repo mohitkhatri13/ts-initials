ffaf9c5d2df2428b5c252ffcc4b4f5a38f284aa2
mohit khatri <khatrimohit2002@gmail.com>
2025-09-30 02:07:02 +0530

    readme

---
+++ node-app/a.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let x = 1;
+console.log(x);
+function greet(a) {
+    console.log(`"Hello" , ${a}`);
+}
+greet("mohit");
+//# sourceMappingURL=a.js.map
---
+++ node-app/a.ts
@@ -0,0 +1,13 @@
+
+let x : number = 1;
+
+console.log(x);
+
+function greet( a:string){
+    console.log(`"Hello" , ${a}`);
+}
+greet("mohit");
+
+
+// types = string , number , bool , any 
+
